a77f4a40beb1ee0f2c760991cf34c0ef7e198280
Jared Folkins <jfolkins@gmail.com>
2012-02-08 08:46:11 -0800

    passed message to background.html so that dog.run() wouldn't die when popup closed

--- dog.js
+++ dog.js
@@ -1,17 +1,18 @@
 function Dog() {
   this._DEBUG = 1;
-  this._currentUsage = '0';
-  this._monthlyCap = '0';
+  this._currentUsage = localStorage['usage'] || 0;
+  this._monthlyCap = localStorage['cap'] || 0;
 
   this.log = function(text) {
-    if(this._DEBUG == 1) {
+    if(dog._DEBUG == 1) {
       console.log(text);
     }
-  }
+  };
 
   this.setUsageBadge = function() {
+    dog.log('setUsageBadge() Current Usage = ' + dog._currentUsage);
     chrome.browserAction.setBadgeText({
-      text: String( this._currentUsage + 'g' )
+      text: String( dog._currentUsage + 'g' )
     });
     chrome.browserAction.setBadgeBackgroundColor({
       color: [0, 0, 0, 255]
@@ -19,6 +20,7 @@ function Dog() {
   };
 
   this.setUsageBadgeError = function() {
+    dog.log('setUsageBadgeError');
     chrome.browserAction.setBadgeText({
       text: String('error')
     });
@@ -27,29 +29,46 @@ function Dog() {
     });
   };
 
+  this.notify = function() {
+    // Create a simple text notification:
+    var pattern = /(\d{2})/g
+    var target = dog._monthlyCap / dog._currentUsage * 10;
+    var results = pattern.exec(target);
+    var percentage = results[1] || 0;
+    if(percentage >= 70) {
+      var message = 'You have used ' + percentage + '% of your monthly alloment.';
+      var notification = webkitNotifications.createNotification(
+        '48x48.png',  // icon url - can be relative
+        'Usage Alert!',  // notification title
+        message
+      );
+      notification.show();
+    }
+  };
 
   this.setBadge = function(text) {
     var pattern = /Current\sUsage:\s\<\/strong\>(\d*)\.\d\sGB\/(\d*)\sGB\<\/td\>/g;
     var results = pattern.exec(text);
     if(results) {
-      this._currentUsage = results[1];
-      this._monthlyCap = results[2];
-      this.setUsageBadge();
+      dog._currentUsage = results[1];
+      dog._monthlyCap = results[2];
+      dog.setUsageBadge();
     } else {
-      this.setUsageBadgeError();
-      this.unsetLocalStoragePassword();
+      dog.setUsageBadgeError();
+      dog.unsetLocalStoragePassword();
     }
-  }
+  };
 
   this.unsetLocalStoragePassword = function() {
     localStorage['password'] = '';
-  }
+  };
+
   this.run = function() {
     var email = encodeURIComponent(localStorage['email']);
     var password = encodeURIComponent(localStorage['password']);
     if(email != 'undefined' && password != 'undefined') {
-      this.log(email);
-      this.log(password);
+      dog.log(email);
+      dog.log(password);
       var url = "https://secure.bendbroadband.com/usage/usage.asp?action=submit&pageID=mbb&subID=hsiu&adct=3";
       var params = 'Submit1=View+Usage&email=' + email + '&password=' + password;
       var xhr = new XMLHttpRequest();
@@ -58,17 +77,18 @@ function Dog() {
       xhr.send(params);
       xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
-          var dog = new Dog();
-          dog.log(xhr.responseText);
+          //dog.log(xhr.responseText);
           dog.setBadge(xhr.responseText);
         }
       }
     }
-  }
+  };
 
   this.init = function() {
-    this.run();
-    setInterval("this.run()", 1000*60*60*8);
-  }
+    dog.run();
+    //if(localStorage['fetchInterval']) ?  = local
+    setInterval("dog.run()", 1000*60*60*8);
+    setInterval("dog.notify()", 1000*60*60*24);
+  };
 
-}
+};

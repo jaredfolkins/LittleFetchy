14126a9af304d2d7f12c5b4659bfbac244fab85c
Jared Folkins <jfolkins@gmail.com>
2016-04-07 15:50:06 -0700

    enhc: added basic templating system

---
+++ templates/bendbroadband.js
@@ -0,0 +1,9 @@
+{
+  "isp"           : "bendbroadband",
+  "postUrl"       : "https://bendbroadband.com/usage/?action=submit",
+  "getUrl"        : "https://bendbroadband.com",
+  "userParam"     : "uaddress",
+  "passParam"     : "upassword",
+  "formRegex"     : "<p><strong>Current\\sUsage\\:\\s<\\/strong>\\s(\\d*)\\.?\\d?\\d?\\sGB\\/(\\d*)\\sGB<\\/p>",
+  "formModifier"  : "g"
+}

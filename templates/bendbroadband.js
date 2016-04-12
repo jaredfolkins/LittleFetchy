922540657f09cb0eb3d7f7635683718292325cc9
Jared Folkins <jfolkins@gmail.com>
2016-04-11 22:25:32 -0700

    fix: regex and maths

--- templates/bendbroadband.js
+++ templates/bendbroadband.js
@@ -4,6 +4,6 @@
   "getUrl"        : "https://bendbroadband.com",
   "userParam"     : "uaddress",
   "passParam"     : "upassword",
-  "formRegex"     : "<p><strong>Current\\sUsage\\:\\s<\\/strong>\\s([0-9]*)\\.?[0-9]{2}?\\sGB\\/([0-9]*)\\sGB<\\/p>",
+  "formRegex"     : "<p><strong>Current\\sUsage\\:\\s<\\/strong>\\s([0-9]*)\\.?[0-9]{1,2}?\\sGB\\/([0-9]*)\\sGB<\\/p>",
   "formModifier"  : "g"
 }
